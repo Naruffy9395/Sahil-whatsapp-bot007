3de8cb490f51db301ebbd0c988ee124ec306a6da
Naruffy9395 <sahilsaroj9395@gmail.com>
2026-06-30 21:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request
+import requests
+import google.generativeai as genai
+import os
 
+app = Flask(__name__)
+
+# ==========================
+# Environment Variables
+# ==========================
+
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Gemini Setup
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-2.5-flash")
